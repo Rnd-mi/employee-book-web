c4123d18bdecbd26dfb05730af1f31d36700ca11
Rndmi <SireOfAll79@yandex.ru>
2023-05-10 11:28:40 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course2/hometask0905/Application.java
@@ -0,0 +1,12 @@
+package pro.sky.course2.hometask0905;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
